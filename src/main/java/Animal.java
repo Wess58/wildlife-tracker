f2ce0e6e93d2b94ec92ebec410da2c6a0261bf27
Wess58 <wesleymbate@gmail.com>
2018-10-23 11:54:21 +0300

    created getters & methods for Animal class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -24,5 +24,85 @@ public class Animal {
 
     }
 
-  
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRanger() {
+        return ranger;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Timestamp getSpotted() {
+        return spotted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public static List<Animal> all() {
+        String sql = "SELECT name, ranger, age, location, health, status, spotted FROM animals";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Animal.class);
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName()) &&
+                    this.getRanger().equals(newAnimal.getRanger()) &&
+                    this.getAge().equals(newAnimal.getAge()) &&
+                    this.getLocation().equals(newAnimal.getLocation()) &&
+                    this.getHealth().equals(newAnimal.getHealth()) &&
+                    this.getStatus().equals(newAnimal.getStatus()) &&
+                    this.getId() == newAnimal.getId();
+
+                   }
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, ranger, age, location, health, status, spotted ) VALUES (:name, :ranger, :age, :location, :health, :status, now())";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("ranger", this.ranger)
+                    .addParameter("age", this.age)
+                    .addParameter("location", this.location)
+                    .addParameter("health", this.health)
+                    .addParameter("status", this.status)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static Animal find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            Animal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Animal.class);
+            return animal;
+        }
+    }
 }
